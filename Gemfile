7878c63c8a7317e265f34aff488c7b097c03556d
Abubakr A.Hafiz <hebakr@gmail.com>
2019-03-14 11:24:10 +0300

    secrect file

--- Gemfile
+++ Gemfile
@@ -60,11 +60,13 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
+# gem 'sshkit-sudo'
 group :development do
-  gem 'capistrano',         require: false
-  gem 'capistrano-rvm',     require: false
-  gem 'capistrano-rails',   require: false
-  gem 'capistrano-bundler', require: false
-  gem 'capistrano3-puma',   require: false
+    gem 'capistrano',         require: false
+    gem 'capistrano-rvm',     require: false
+    gem 'capistrano-rails',   require: false
+    gem 'capistrano-bundler', require: false
+    gem 'capistrano3-puma',   require: false
+    
 end
+
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,18 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-server '134.209.112.68', port: your_port_num, roles: [:web, :app, :db], primary: true
+server '142.93.55.207', roles: [:web, :app, :db], primary: true
 
 set :application, "cap_demo"
-set :repo_url, "git@example.com:me/cap_demo.git"
+set :repo_url, "git@github.com:hebakr/cat_demo.git"
 
 set :user,            'deployer'
 set :puma_threads,    [4, 16]
-set :puma_workers,    0
+set :puma_workers,    1
 
 # Don't change these unless you know what you're doing
 set :pty,             true
-set :use_sudo,        false
+set :use_sudo,        true
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
@@ -21,7 +21,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
@@ -64,6 +64,7 @@ namespace :deploy do
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
+      puts 'INIT DEPLOY START ************'
       before 'deploy:restart', 'puma:start'
       invoke 'deploy'
     end
